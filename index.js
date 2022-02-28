ca85eaf7118d886a6e37be0f258ef4a8be12a1da
wodouxiangxiaole <85941647+wodouxiangxiaole@users.noreply.github.com>
2022-02-28 09:58:20 -0800

    change index.js

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const express = require('express');
-const { render } = require('express/lib/response');
+const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express()
+  app.use(express.json());
+  app.use(express.urlencoded({extended:false}));
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const { Pool } = require('pg');
-
-var pool;
-// pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl:{
-//     rejectUnauthorized: false
-//   } 
-// })
-pool = new Pool({
-  // for the local host
-  connectionString: 'postgres://postgres:123wzqshuai@localhost/users' 
-})
-
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+  
 app.get('/', (req, res) => res.render('pages/index'));
 
 app.post('/signUp', async (req, res) => {
   var inputEmail = req.body.email;
   var inputPswd = req.body.pswd;
-
+  var inputRepeatPswd = req.body.repeatPswd;
   try {
     await pool.query(`INSERT INTO usrs (umail, upswd)
     VALUES ('${inputEmail}', '${inputPswd}')`);
@@ -43,5 +28,3 @@ app.post('/signUp', async (req, res) => {
   }
 })
 
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
