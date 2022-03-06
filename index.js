59ff82e91e3577c4f4393b6e12e80bdc6fca86cc
Aurora Yang <yya225@sfu.ca>
2022-03-06 14:27:39 -0800

    test pull

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const session = require("express-session")
 const PORT = process.env.PORT || 5000
 
 var app = express()
@@ -64,12 +63,7 @@ app.post('/userlogin', async(req,res) => {
   } 
 })
 
-app.post('/userlogout', async(req,res) => {
-  if(req.body.session.user) {
-    res.session.destory;
-  }
-  res.redirect('/userlogin.html');
-})
+
 
 app.post('/adminlogin', async(req,res) => {
   var uname = req.body.uname;
