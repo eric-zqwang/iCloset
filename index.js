2febd2b4efacdbea9c16b6997ad9bb2f9ac3d6da
wodouxiangxiaole <85941647+wodouxiangxiaole@users.noreply.github.com>
2022-03-07 11:01:22 -0800

    change index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const session = require("express-session")
 const PORT = process.env.PORT || 5000
 
 var app = express()
@@ -40,7 +41,63 @@ app.post('/signUp', async (req, res) => {
       res.redirect('/userlogin.html');
     }
     catch (error) {
-      res.end(error);
+      res.send("User NAME ALREADY EXISTS");
     }
   }
+})
+
+/** change database name "users"; column name "uname", "psw"; "homepage" ...*/
+app.post('/userlogin', async(req,res) => {
+  var uname = req.body.uname;
+  var password = req.body.psw;
+  const data = result.rows;
+
+  //search database using uname
+  const result = await SecurityPolicyViolationEvent.query("SELECT * FROM users WHERE uname='" + uname + "';");
+
+  //If username is not unique
+  if (data.length > 1) {
+    console.log("DUPLICATE USERS!!!");
+  } 
+  //If usename and password are correct, direct to homepage
+  else if (data.length == 1 && password == data[0].psw) {
+    res.render('pages/homepage', data[0])
+  } 
+  
+  //If user does not exist or password is incorrect, alert user
+  else if (data.length == 0 || password != data[0].psw) {
+    window.alert("incorrect username or password");
+  } 
+})
+
+app.post('/userlogout', async(req,res) => {
+  if(req.body.session.user) {
+    res.session.destory;
+  }
+  res.redirect('/userlogin.html');
+})
+
+
+
+app.post('/adminlogin', async(req,res) => {
+  var uname = req.body.uname;
+  var password = req.body.psw;
+  const data = result.rows;
+  
+  //search database using uname
+  const result = await SecurityPolicyViolationEvent.query("SELECT * FROM users WHERE uname='" + uname + "';");
+
+  //If username is not unique
+  if (data.length > 1) {
+    console.log("DUPLICATE USERS!!!");
+  } 
+  //If usename and password are correct, direct to homepage
+  else if (data.length == 1 && password == data[0].psw && data[0].authority == true) {
+    res.render('pages/homepage', data[0])
+  } 
+  
+  //If user does not exist or password is incorrect, alert user
+  else if (data.length == 0 || password != data[0].psw || data[0].authority != true) {
+    window.alert("incorrect username or password");
+  } 
 })
