ed062c5cc32e37749995cb8515dae8f4ce77a1b3
Aurora Yang <yya225@sfu.ca>
2022-03-16 14:44:01 -0700

    add the imagetest.js

--- index.js
+++ index.js
@@ -232,21 +232,3 @@ app.get('/outfit', (req, res) => {
   res.render('pages/outfit-collages');
  });
 
- //remove background
- const {removeBackgroundFromImageUrl,removeBackgroundFromImageFile} = require("remove.bg")
- 
-const localFile = "./image/01.jpg";
-const outputFile = `${__dirname}/out/img-removed-from-file.png`
- 
-removeBackgroundFromImageFile({
-  path: localFile,
-  apiKey: "oZ4aG5Km9m7fCuVQxYYZsGSn",
-  size: "regular",
-  type: "auto",
-  scale: "50%",
-  outputFile 
-}).then((result) => {
- console.log(`File saved to ${outputFile}`);
-}).catch((errors) => {
- console.log(JSON.stringify(errors));
-});
